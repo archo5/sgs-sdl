ffb1e9254b0e2e57d5f674036f94af13064a20bc
snake5 <snake5creator@gmail.com>
2016-09-01 13:50:26 +0300

    added ogg/vorbis to android makefile

--- jni/Android.mk
+++ jni/Android.mk
@@ -21,5 +21,30 @@ LOCAL_SHARED_LIBRARIES := sgscript sgsxgmath
 include $(BUILD_SHARED_LIBRARY)
 
 
+# Ogg
+LIBOGG_ROOT := ../ext/src/libogg-1.3.1
+include $(CLEAR_VARS)
+LOCAL_MODULE := ogg
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(LIBOGG_ROOT)/include
+LOCAL_SRC_FILES := $(LIBOGG_ROOT)/src/bitwise.c $(LIBOGG_ROOT)/src/framing.c
+include $(BUILD_STATIC_LIBRARY)
+
+# Vorbis
+LIBVORBIS_ROOT := ../ext/src/libvorbis-1.3.3
+_LIBVORBIS_SRCNAMES := analysis bitrate block codebook envelope \
+	floor0 floor1 info lookup lpc lsp mapping0 mdct psy \
+	registry res0 sharedbook smallft synthesis vorbisenc vorbisfile window
+include $(CLEAR_VARS)
+LOCAL_MODULE := vorbis
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(LIBOGG_ROOT)/include $(LOCAL_PATH)/$(LIBVORBIS_ROOT)/include $(LOCAL_PATH)/$(LIBVORBIS_ROOT)/lib
+LOCAL_SRC_FILES := $(patsubst %,$(LIBVORBIS_ROOT)/lib/%.c,$(_LIBVORBIS_SRCNAMES))
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := oggvorbistest
+LOCAL_STATIC_LIBRARIES := ogg vorbis
+include $(BUILD_SHARED_LIBRARY)
+
+
 # SGScript
 include ../sgscript/jni/Android.mk
